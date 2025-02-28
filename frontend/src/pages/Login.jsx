2a9b8dba6c86b7e592994fedaa932484f869a0c6
moin <moinnakhwaji@gmail.com>
2025-02-28 14:34:09 +0530

    contact, login and signin

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import { useState } from "react";
+// import { useAuth } from "../AuthContext";
+import { useNavigate } from "react-router-dom";
+import { Navbar } from "../components";
+import styles from "../style";
+import "./Spinner.css";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    // const { login } = useAuth();
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
-export default Login
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setError("");
+        try {
+            // await login(username, password);
+            navigate("/loggedinhome");
+        } catch (err) {
+            setError("Invalid username or password");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <>
+            <div className={`${styles.paddingX} ${styles.flexCenter} bg-[#0a0a0a]`}>
+                <div className={`${styles.boxWidth}`}>
+                    <Navbar />
+                </div>
+            </div>
+
+            <div className="bg-[#0a0a0a] text-white font-sans min-h-screen flex items-center justify-center">
+                <div className="bg-[#141414] w-full max-w-md p-8 rounded-xl shadow-2xl">
+                    <h1 className="text-3xl font-bold text-center mb-6 text-[#f1f1f1]">Welcome Back!</h1>
+
+                    <form className="space-y-6" onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor="username" className="block text-sm font-medium mb-2">
+                                Username
+                            </label>
+                            <input
+                                type="text"
+                                id="username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                className="w-full bg-[#0a0a0a] border border-transparent rounded-md px-4 py-3 text-sm focus:ring-2 focus:ring-[#6556cd] focus:outline-none placeholder-gray-400"
+                                placeholder="Enter your username"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="block text-sm font-medium mb-2">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="w-full bg-[#0a0a0a] border border-transparent rounded-md px-4 py-3 text-sm focus:ring-2 focus:ring-[#6556cd] focus:outline-none placeholder-gray-400"
+                                placeholder="Enter your password"
+                                required
+                            />
+                        </div>
+                        {error && <p className="text-red-500 text-center">{error}</p>}
+                        <button
+                            type="submit"
+                            className="w-full bg-white text-black py-3 rounded-md hover:bg-gray-300 focus:outline-none transition duration-300 flex justify-center items-center"
+                            disabled={loading}
+                        >
+                            {loading ? <span className="spinner"></span> : "Login"}
+                        </button>
+                    </form>
+                    <div className="mt-8 text-center">
+                        <p className="text-sm text-gray-400">
+                            Don't have an account?{" "}
+                            <span
+                                onClick={() => navigate("/signin")}
+                                className="cursor-pointer text-[#6556cd] hover:underline"
+                            >
+                                Sign Up
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Login;
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,9 +1,125 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// import { useAuth } from "../AuthContext";
+import { Navbar } from "../components";
+import styles from '../style';
+import "./Spinner.css"
 
 const Signin = () => {
-  return (
-    <div>Signin</div>
-  )
-}
+    // const { signup } = useAuth();
+    const [loading, setLoading] = useState(false); // Added loading state
+    const navigate = useNavigate();
 
-export default Signin
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const [error, setError] = useState("");
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setError("");
+
+        try {
+            // await signup(
+            //     formData.username,
+            //     formData.password,
+            //     formData.confirmPassword
+            // );
+
+            navigate("/create-profile"); // Proceed to profile creation
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false); // Set loading to false after the process completes (for both login and signup)
+        }
+    };
+
+
+    return (
+      <>
+            <div className={`${styles.paddingX} ${styles.flexCenter} bg-primary`}>
+                <div className={`${styles.boxWidth}`}>
+                    <Navbar />
+                </div>
+            </div>
+
+            <div className="bg-[#0a0a0a] text-white font-sans min-h-screen flex items-center justify-center">
+                <div className="bg-[#141414] w-full max-w-md p-8 rounded-xl shadow-lg">
+                    <h1 className="text-3xl font-bold text-center mb-6 text-white">Create Account</h1>
+                    <form className="space-y-6" onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor="username" className="block text-sm font-medium mb-2">
+                                Username
+                            </label>
+                            <input
+                                type="text"
+                                name="username"
+                                id="username"
+                                value={formData.username}
+                                onChange={handleChange}
+                                className="w-full bg-[#1f1e24] border border-gray-700 rounded-md px-4 py-3 text-sm focus:ring-2 focus:ring-purple-500 focus:outline-none placeholder-gray-400"
+                                placeholder="Enter your username"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="block text-sm font-medium mb-2">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                className="w-full bg-[#1f1e24] border border-gray-700 rounded-md px-4 py-3 text-sm focus:ring-2 focus:ring-purple-500 focus:outline-none placeholder-gray-400"
+                                placeholder="Enter your password"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="confirmPassword" className="block text-sm font-medium mb-2">
+                                Confirm Password
+                            </label>
+                            <input
+                                type="password"
+                                id="confirmPassword"
+                                name="confirmPassword"
+                                value={formData.confirmPassword}
+                                onChange={handleChange}
+                                className="w-full bg-[#1f1e24] border border-gray-700 rounded-md px-4 py-3 text-sm focus:ring-2 focus:ring-purple-500 focus:outline-none placeholder-gray-400"
+                                placeholder="Confirm your password"
+                                required
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full bg-white text-black py-3 rounded-md hover:bg-gray-400 focus:outline-none transition duration-300 flex justify-center items-center"
+                            disabled={loading}
+                        >
+                            {loading ? <span className="spinner"></span> : "Sign Up"}
+                        </button>
+                    </form>
+                    <div className="mt-8 text-center">
+                        <p className="text-sm text-gray-400">
+                            Already have an account?{" "}
+                            <span onClick={() => navigate("/login")} className="cursor-pointer text-purple-500 hover:underline">
+                                Login
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Signin;
