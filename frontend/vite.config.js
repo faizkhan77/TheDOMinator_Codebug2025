87f887f5951c268fdc4d02c2b6116aa3475e7e5e
moin <moinnakhwaji@gmail.com>
2025-02-28 10:58:17 +0530

    configured tailwindcss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // host: '0.0.0.0',  // Exposes the app to the local network
+    // port: 5173,        // You can change this if needed
+    proxy: {
+      '/api': {
+          target: 'http://127.0.0.1:8000', // Your backend server
+          changeOrigin: true,
+          secure: false,
+      },
+  },
+  }
 })
